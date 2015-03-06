d96374e5ed719d2b63db60332d3f30ec37a918f1
Ryan Milvenan <ryanmilvenan@gmail.com>
2015-03-06 12:29:51 -0700

    Initial commit

---
+++ contents/js/app.js
@@ -0,0 +1,18 @@
+$( "#legislators" ).on( "click", function( event ) {
+    legislators.load()
+})
+
+$( "#districts" ).on( "click", function( event ) {
+    districts.load()
+})
+
+$( "#committees" ).on( "click", function( event ) {
+    committees.load()
+})
+$( "#votes" ).on( "click", function( event ) {
+    votes.load()
+})
+
+$( "#bills" ).on( "click", function( event ) {
+    bills.load()
+})
