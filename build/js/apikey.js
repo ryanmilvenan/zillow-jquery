f34de0bad7cd95e23a9055473107ddcd80ace4bd
Ryan Milvenan <ryanmilvenan@gmail.com>
2015-03-06 19:03:01 -0700

    build"

--- build/js/apikey.js
+++
@@ -1,2 +0,0 @@
-// api key
-var apikey = 'X1-ZWz1axwznz2xhn_6le3g'
--- build/js/app.js
+++
@@ -1,7 +0,0 @@
-$( "#properties" ).on( "click", function( event ) {
-    properties.load()
-})
-
-$( "#neighborhood" ).on( "click", function( event ) {
-    neighborhood.load()
-})
